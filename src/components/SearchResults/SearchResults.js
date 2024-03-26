805968f412620fa06865e210aa3e2d52bd8e8fc5
techStarZx <stechstar.me@gmail.com>
2024-03-26 16:58:01 +0500

    Create SearchResults Component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -4,6 +4,7 @@ function SearchResults() {
     return (
         <div className='results'>
             <h2>Results</h2>
+            <div>tracklist goes here</div>
         </div>
     );
 }
---
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { SearchResults } from "./SearchResults";
+
+function SearchResultsContainer() {
+    return <SearchResults />;
+}
+
+export {SearchResultsContainer};
