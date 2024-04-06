b13cde20ed17e83680b10ad12a5c7f6f463e05c9
techStarZx <stechstar.me@gmail.com>
2024-04-06 20:14:17 +0500

    update syntax from .then to async-await

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -22,27 +22,26 @@ const Spotify = {
         window.location = accessUrl;
     },
 
-    search(term) {
+    async search(term) {
         const accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+        const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
             {
                 headers: {
                     Authorization: `Bearer ${accessToken}`
                 }
-            })
-            .then(response => response.json())
-            .then(jsonResponse => {
-                if(!jsonResponse.tracks) {
-                    return [];
-                }
-                return jsonResponse.tracks.items.map(track => ({
-                    id: track.id,
-                    name: track.name,
-                    artist: track.artists[0].name,
-                    album: track.album.name,
-                    uri: track.uri
-                }));
             });
+        const jsonResponse = await response.json();
+        if (!jsonResponse.tracks) {
+            return [];
+        }
+        const tracks = jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        return tracks;
     }
 };
 
