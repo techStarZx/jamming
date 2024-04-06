c6e2f484f819ec81c2dd7a801d7dec8471a20510
techStarZx <stechstar.me@gmail.com>
2024-04-06 15:34:02 +0500

    write function Spotify.getAccessToken

---
+++ src/util/Spotify.js
@@ -0,0 +1,26 @@
+const clientId = '19dafe84f4b94a0baa7931cc61e9446e';
+const redirectUri = 'http://localhost:3000/'; // Have to add this to your accepted Spotify redirect URIs on Spotify API.
+let accessToken;
+
+const Spotify = {
+getAccessToken () {
+    if(accessToken) {
+        return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if(accessTokenMatch && expiresInMatch) {
+        accessToken = accessTokenMatch[1];
+        const expiresIn = expiresInMatch[1];
+        setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+        return accessToken;
+    }
+
+    const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+    window.location = accessUrl;
+}
+};
+
+export default Spotify;
