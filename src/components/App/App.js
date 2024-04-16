60b2ef32cf75f7a346c6b4501b126ddfe194f298
techStarZx <stechstar.me@gmail.com>
2024-04-16 14:24:24 +0500

    Add background image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import styles from './App.module.css';
 import React from 'react';
 import { SearchBarContainer } from '../SearchBar/SearchBarContainer';
 import { SearchResultsContainer } from '../SearchResults/SearchResultsContainer';
@@ -7,7 +7,7 @@ import Header from '../Header/Header';
 
 function App({ trackListForResults, trackListForPlaylist, playlistName, handleClickAddButton, handleClickRemoveButton, handleChangePlaylistNameInput, handleClickSavePlaylistButton, handleClickSearchButton, handleChangeSearchInput, searchTerm }) {
   return (
-    <div className="App">
+    <div className={styles.app}>
       <Header />
       <SearchBarContainer
         handleClickSearchButton={handleClickSearchButton}
